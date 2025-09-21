d0b5e651be0de95c798d50a71e9eb03b172ee8ce
galamo <galamouyal88@gmail.com>
2025-09-21 11:46:17 +0300

    a

--- examples/Js-Refresh/async/callback/index.js
+++ examples/Js-Refresh/async/callback/index.js
@@ -1,5 +1,39 @@
 console.log("Script start");
 
-getPilotDataApi(callbackFunction);
+function getPilotDataApi(callbackFunction) {
+    setTimeout(() => {
+        if (typeof callbackFunction === 'function') {
+            callbackFunction("here is your data!")
+        }
+    }, 5000);
+}
+
+
+getPilotDataApi((data) => {
+    console.log(data)
+});
+getPilotDataApi((data) => {
+    console.log(data)
+});
+
+getPilotDataApi((data) => {
+    console.log(data)
+    getPilotDataApi((data) => {
+        console.log(data)
+        getPilotDataApi((data) => {
+            console.log(data)
+            getPilotDataApi((data) => {
+                console.log(data)
+            });
+        });
+    });
+});
+
+getPilotDataApi((data) => {
+    console.log(data)
+});
+
+
+
 
 console.log("Script end");
