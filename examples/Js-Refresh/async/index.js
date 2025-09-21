8c1743dbab2651bc91738e58a45f90cf276a2c58
galamo <galamouyal88@gmail.com>
2025-09-21 13:33:25 +0300

    a

--- examples/Js-Refresh/async/index.js
+++ examples/Js-Refresh/async/index.js
@@ -1,13 +1,13 @@
 // what will be printed 
 console.log("999")
-async function getApi1(){
+async function getApi1() {
     console.log("111")
-    const result = await fetch("http://elbit1.co.il")
+    const result = await fetch("http://rf1.co.il")
     console.log("767")
 }
-async function getApi2(){
+async function getApi2() {
     console.log("222")
-    const result = await fetch("http://elbit1.co.il")
+    const result = await fetch("http://rf2.co.il")
     console.log("555")
 }
 getApi2()
