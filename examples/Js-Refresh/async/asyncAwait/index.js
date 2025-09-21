b1d11379155005854927ec4e3db4559f1bedd986
galamo <galamouyal88@gmail.com>
2025-09-21 13:26:56 +0300

    a

--- examples/Js-Refresh/async/asyncAwait/index.js
+++ examples/Js-Refresh/async/asyncAwait/index.js
@@ -1,18 +1,29 @@
 console.log("Start");
 
-// implement
-// async function getPilotDataApi(aircraft: string){}
+
+function getPilotDataApi(param) {
+  return new Promise((resolve, reject) => {
+    if (!param) reject("Missing paramter!")
+    setTimeout(() => {
+      resolve("here is your data!")
+    }, 5000);
+  })
+}
+
 
 async function init() {
   try {
     const result = await getPilotDataApi("F-35");
+    const multipleResult = await Promise.all([getPilotDataApi("a"), getPilotDataApi("a")])
+    console.log(multipleResult)
     console.log(result);
-    await getPilotDataApi("F-16");
   } catch (error) {
     console.log(error);
   }
 }
 
 init();
+
+
 
 console.log("End");
