f3af6a22380303156507b9cef8298b0f1f06794b
galamo <galamouyal88@gmail.com>
2025-09-21 11:59:20 +0300

    a

--- examples/Js-Refresh/async/promise/index.js
+++ examples/Js-Refresh/async/promise/index.js
@@ -1,11 +1,28 @@
 console.log("Script start");
 
-// getPilotDataApi("F-16")
-//   .then((dataFromPromise) => {
-//     console.log(dataFromPromise);
-//   })
-//   .catch((error) => {
-//     console.log(error);
-//   });
+function getPilotDataApi(param) {
+    return new Promise((resolve, reject) => {
+        if (!param) reject("Missing paramter!")
+        setTimeout(() => {
+            resolve("here is your data!")
+        }, 5000);
+    })
+}
+
+getPilotDataApi("F-16")
+    .then((dataFromPromise) => {
+        console.log(dataFromPromise);
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+
+getPilotDataApi()
+    .then((dataFromPromise) => {
+        console.log(dataFromPromise);
+    })
+    .catch((error) => {
+        console.log(error);
+    })
 
 console.log("Script end");
