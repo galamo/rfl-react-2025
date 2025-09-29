e991e7d62c8ca4773fac4018873f5cea441722c9
galamo <galamouyal88@gmail.com>
2025-09-29 09:51:37 +0300

    a

--- examples/Js-Refresh/typescript-playground/2/dist/index.js
+++ examples/Js-Refresh/typescript-playground/2/dist/index.js
@@ -12,7 +12,12 @@ function aggregateByCurrency(data) {
 }
 const currency = { code: "ILS", value: 1, symbol: "ILS" };
 aggregateByCurrency([
-    { population: "10m", name: "israel", code: "ISR", currencies: { "ILS": { value: 1, symbol: "#" } } }
+    {
+        population: "10m",
+        name: "israel",
+        code: "ISR",
+        currencies: { ILS: { value: 1, symbol: "#" } },
+    },
 ]);
 // function User(_value: number, _symbol: string): void {
 //     this.value = _value
@@ -24,5 +29,35 @@ function getUser() {
     return { name: "galamo" };
 }
 console.log(getUser());
-function getTax(value) {
+function getTax(value, tax) {
+    if (typeof value === 'string') {
+        const n = parseInt(value) * tax;
+        return n;
+    }
+    else {
+        return value * tax;
+    }
 }
+function createany(url, userOrId) {
+}
+function getDayFromDate(date) {
+    if (new Date(date).getDay() + 1 === 1)
+        return "Sunday";
+    if (new Date(date).getDay() + 1 === 2)
+        return "Monday";
+}
+getDayFromDate(new Date().toDateString());
+function getSingleUser(users) {
+    return users[0];
+}
+function getSingleProduct(products) {
+    return products[0];
+}
+const b = getSingleUser(["user1", "user_2", "user__3"]);
+async function getUserFromApi(id) {
+    return { userName: "Ehud", age: 32, dbId: 1 };
+}
+function getWithID(id) {
+    return { a: 1, dbId: 1 };
+}
+const fromDb = getWithID(1);
--- examples/Js-Refresh/typescript-playground/2/src/index.ts
+++ examples/Js-Refresh/typescript-playground/2/src/index.ts
@@ -1,41 +1,40 @@
 import data from "./singleCountry.json";
 console.log(data);
-import { test } from "./a";
 type SingleCountry = typeof data;
 
 type Country = {
-  name: string;
-  code: string;
-  population: string;
-  currencies: {
-    [key: string]: { value: number; symbol: string };
-  };
+    name: string;
+    code: string;
+    population: string;
+    currencies: {
+        [key: string]: { value: number; symbol: string };
+    };
 };
 
 function aggregateByCurrency(data: Array<Country>) {
-  if (!Array.isArray(data)) return;
-  return data.filter((c) => c.name === "israel");
+    if (!Array.isArray(data)) return;
+    return data.filter((c) => c.name === "israel");
 }
 type ParitalCountry = Required<Partial<Country>>;
 
 interface Currency {
-  symbol: string;
-  value: number;
+    symbol: string;
+    value: number;
 }
 
 interface Currency {
-  code: string;
+    code: string;
 }
 
 const currency: Currency = { code: "ILS", value: 1, symbol: "ILS" };
 
 aggregateByCurrency([
-  {
-    population: "10m",
-    name: "israel",
-    code: "ISR",
-    currencies: { ILS: { value: 1, symbol: "#" } },
-  },
+    {
+        population: "10m",
+        name: "israel",
+        code: "ISR",
+        currencies: { ILS: { value: 1, symbol: "#" } },
+    },
 ]);
 
 // function User(_value: number, _symbol: string): void {
@@ -45,27 +44,74 @@ aggregateByCurrency([
 // const user = new User(1, "aa")
 // console.log(user)
 function getUser() {
-  return { name: "galamo" };
+    return { name: "galamo" };
 }
 console.log(getUser());
 
 type AggregateCountry = Pick<SingleCountry, "ccn3" | "currencies" | "flags">;
 type AggregateCountry1 = Omit<SingleCountry, "ccn3">;
 
-function getTax(value: string | number) {}
-// continue here - narrow down.
-interface User {
-  id: number;
-  name: string;
-  email: string;
-  age: number;
+function getTax(value: string | number, tax: number): number {
+    if (typeof value === 'string') {
+        const n = parseInt(value) * tax
+        return n;
+    } else {
+        return value * tax
+    }
 }
 
-// keyof creates a union type of all property keys
-type UserKeys = keyof User; // "id" | "name" | "email" | "age"
-function getProp<T, K extends keyof T>(a: T, b: K): T[K] {
-  return a[b];
+
+function createany(url: string, user: string): any;
+function createany(url: string, userIdPassword: number): any;
+
+function createany(url: string, userOrId: string | number): any {
 }
-const key1: UserKeys = "name";
-const key2: UserKeys = "email";
-const key3: UserKeys = "";
+
+type DayOfWeeks =
+    | "Sunday"
+    | "Monday"
+    | "Tuesday"
+    | "Wednesday"
+    | "Thursday"
+    | "Friday"
+    | "Saturday";
+
+function getDayFromDate(date: string): DayOfWeeks | undefined {
+    if (new Date(date).getDay() + 1 === 1) return "Sunday";
+    if (new Date(date).getDay() + 1 === 2) return "Monday";
+
+}
+
+
+
+
+getDayFromDate(new Date().toDateString())
+
+
+
+function getSingleUser(users: string[]): string {
+    return users[0];
+}
+
+
+function getSingleProduct(products: number[]): number {
+    return products[0];
+}
+
+const b = getSingleUser(["user1", "user_2", "user__3"])
+
+type User = { userName: string, age: number }
+
+async function getUserFromApi(id: number): Promise<User & { dbId: number }> {
+    return { userName: "Ehud", age: 32, dbId: 1 }
+}
+type WithId = { dbId: number }
+function getWithID<T>(id: number): T & WithId {
+    return { a: 1, dbId: 1 }
+}
+const fromDb = getWithID<User>(1)
+
+type FnReturn = Awaited<ReturnType<typeof getUserFromApi>>
+type FnReturn1 = ReturnType<typeof getUserFromApi>
+
+
