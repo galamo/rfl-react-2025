595ae0cbf74120c8c83b216c318fac0d94f75a79
galamo <galamouyal88@gmail.com>
2025-10-20 10:04:44 +0300

    countries-api

--- react/app_3/my-react-app/src/components/pages/countries/index.tsx
+++ react/app_3/my-react-app/src/components/pages/countries/index.tsx
@@ -1,21 +1,45 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import type { ChangeEvent } from "react";
 import { HeaderApp } from "../../header-app";
 import data from "./dummyData.json";
 import css from "./countries.module.css";
 import { CountryCard } from "./card";
+import axios from "axios";
+
 type SingleCountry = (typeof data)[0];
 
 export function CountriesPage() {
   const [filter, setFilter] = useState("");
+  const [countries, setCountries] = useState<Array<SingleCountry>>([]);
+  const [isLoadingCountries, setIsLoadingCountries] = useState(false);
   function handleFilter(e: ChangeEvent<HTMLInputElement>) {
     setFilter(e.target.value);
   }
-  const countries = filter
-    ? data.filter((item) =>
-        item?.name?.common?.toLowerCase().includes(filter.toLowerCase())
-      )
-    : data;
+
+  useEffect(() => {
+
+    async function getCountries() {
+      try {
+        // temp
+        setIsLoadingCountries(true)
+        const url = "http://localhost:3000/api/data/countries-rfl"
+        const result = await axios.get(url);
+        setCountries(result.data as Array<SingleCountry>)
+      } catch (ex: any) {
+        console.log(ex)
+      } finally {
+        setIsLoadingCountries(false)
+      }
+    }
+    getCountries()
+
+  }, [])
+
+  const filteredCountries = filter
+    ? countries.filter((item: SingleCountry) =>
+      item?.name?.common?.toLowerCase().includes(filter.toLowerCase())
+    )
+    : countries;
   return (
     <>
       <div>
@@ -25,8 +49,9 @@ export function CountriesPage() {
         </div>
         <input type="text" onChange={handleFilter} />
       </div>
+      {isLoadingCountries ? <h2> Loading... </h2> : null}
       <div className={css.cardsWrapper}>
-        {countries.map((item: SingleCountry) => {
+        {filteredCountries.map((item) => {
           return (
             <CountryCard
               key={item.name.common}
